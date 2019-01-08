5257dd0b677cc3afae3b24ef381520b560224dcc
Dirk Tornow <thrvr@thrvrs.com>
2019-01-08 09:10:21 +0100

    added python requests install to the setup

--- python_pip/install.sh
+++ python_pip/install.sh
@@ -2,14 +2,31 @@
 #
 # Installing pip 
 #
+cdir=`pwd`
+# Check for pip
+# if test ! $(which pip)
+# then
+#   echo "  Installing `pip` for you."
+#   sudo easy_install pip
+# else
+#   echo "  pip already installed. Checking for updates..."
+#   sudo -H pip install --upgrade pip
+# fi
 
-# Check for Homebrew
-if test ! $(which pip)
+present=`pip list | grep requests | sed 's/^requests\ *//'`
+
+if test ! $(echo $present)
 then
-  echo "  Installing `pip` for you."
-  sudo easy_install pip
+	echo "   python package requests not found. Cloning git and installing it."
+	mkdir -p /tmp/requests
+	cd /tmp/requests
+	git clone git://github.com/requests/requests.git
+	cd requests
+	pip install requests --user
+	cd /tmp
+	rm -rf requests
+	cd $cdir
 else
-  echo "  pip already installed. Checking for updates..."
-  sudo -H pip install --upgrade pip
+	echo "   found requests in version $present"
+	echo "   skipping the installation. If there is a newer version, we'll upgrade that automatically in a bit..."
 fi
-
