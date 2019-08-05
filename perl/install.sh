bf6aa705699283f28fefb9616928b3d034a2182e
Dirk Tornow <thrvr@thrvrs.com>
2019-08-05 08:43:16 +0200

    added mongodb perl modules and installaiton file

---
+++ perl/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+/usr/local/bin/cpan -i SUPER
+/usr/local/bin/cpan -i Algorithm::C3
+/usr/local/bin/cpan -i Test::LeakTrace
+/usr/local/bin/cpan -i Test::Pod
+/usr/local/bin/cpan -i Test::Output
+/usr/local/bin/cpan -i Log::Dispatch
+/usr/local/bin/cpan -i Log:Log4perl
+/usr/local/bin/cpan -i Moo
+/usr/local/bin/cpan -i Mouse
+/usr/local/bin/cpan -i namespace
+/usr/local/bin/cpan -i MooseX
+/usr/local/bin/cpan -i Moose
+/usr/local/bin/cpan -i Type::Library
+/usr/local/bin/cpan -i Type::Utils
+/usr/local/bin/cpan -i Types::Standard
+/usr/local/bin/cpan -i Authen::SCRAM::Client
+/usr/local/bin/cpan -i MongoDB
+/usr/local/bin/cpan -i Data::Dumper
