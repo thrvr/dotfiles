cf9dcb104da199cd625ab6767391ec61531e3169
Dirk Tornow <dirk@thrvrs.com>
2019-10-16 08:10:19 +0200

    optimized prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -51,7 +51,8 @@ need_push () {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  # echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  echo "%(!.%F{red}.%F{cyan})%n%f@%F{yellow}%m%f%(!.%F{red}.)  '%{$(pwd|grep --color=always /)%${#PWD}G%}'"
 }
 
 battery_status() {
@@ -61,7 +62,11 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+#export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+
+export PROMPT=$'$(directory_name) $(git_dirty)$(need_push)\n› '
+
+# export PROMPT='%(!.%F{red}.%F{cyan})%n%f@%F{yellow}%m%f%(!.%F{red}.) %{$(pwd|grep --color=always /)%${#PWD}G%}'
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
