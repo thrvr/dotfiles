4d91c1b04a7c9d2ee8b6fc137054dc81349dd855
Dirk Tornow <dirk@thrvrs.com>
2020-01-09 12:07:33 +0100

    more corrections and more documentation

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -3,5 +3,5 @@
 # this installation script automatically installs vagrant additions 
 # that are needed for the provisioning of the development vagrant box
 #
-vagrant plugin install vagrant-hostmanager
-vagrant plugin install vagrant-disksize 
+command -v vagrant && vagrant plugin install vagrant-hostmanager
+command -v vagrant && vagrant plugin install vagrant-disksize 
