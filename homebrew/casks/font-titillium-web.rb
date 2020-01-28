fd655885ad86e582c7a7c324778c2a7007af5f28
Dirk Tornow <dirk@thrvrs.com>
2020-01-28 08:14:05 +0100

    added bcrypt

--- homebrew/casks/font-titillium-web.rb
+++ homebrew/casks/font-titillium-web.rb
@@ -1,4 +1,4 @@
-cask 'font-tillium-web' do
+cask 'font-titillium-web' do
   sha256 '2a96c82fbabd61b37640054b20d52483817d2c7661ce7e82faccdfb5b436f1bf'
 
   url "https://google-webfonts-helper.herokuapp.com/api/fonts/titillium-web?download=zip&subsets=latin&variants=regular"
