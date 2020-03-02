a4fb184016ba57d6df626d2953ed770f9a69714b
Dirk Tornow <dirk@thrvrs.com>
2020-03-02 07:24:54 +0100

    added updates for Vagrant for Kisura

--- python_pip/install.sh
+++ python_pip/install.sh
@@ -15,11 +15,58 @@ cdir=`pwd`
 # fi
 
 
+# pcregrep -v $exclude_pattern for excluding these packages
+# form upgrading
+exclude_pattern='(pip|docutils|idna)'
+
+# versions that we freeze packages at
+pip_version=20.0.2
+docutils_version=0.15.2
+idna_version=2.8
+
+echo "#-----------------------------------#"
+echo "# Installing fixed version packages #"
+echo "#-----------------------------------#"
+
+# Installing `pip` in required version 
+#
+present=`pip list | grep pip | sed -E 's/^pip\ *//' | sed -E 's/\ //g'`
+if [ "$present" != "$pip_version" ]; then
+	echo "Found pip in version '$present' vs. needed '$pip_version' "
+	pip install pip==$pip_version --user
+else
+	echo "   found pip in version in required version $present"
+fi
+
+# Installing `docutils` 
+#
+present=`pip list | grep docutils | sed -E 's/^docutils\ *//' | sed -E 's/\ //g'`
+if [ "$present" != "$docutils_version" ]; then
+	echo "Found docutils in version '$present' vs. needed '$idna_version' "
+	pip install docutils==$docutils_version --user
+else
+	echo "   found docutils in version in required version $present"
+fi
+
+# Installing `idna` 
+#
+present=`pip list | grep idna | sed -E 's/^idna\ *//' | sed -E 's/\ //g'`
+if [ "$present" != "$idna_version" ]; then
+	echo "Found idna in version '$present' vs. needed '$idna_version' "
+	pip install idna==$idna_version --user
+else
+	echo "   found idna in version in required version $present"
+fi
+
+echo "#------------------------------------#"
+echo "# Installing latest version packages #"
+echo "#------------------------------------#"
+
+
 # Installing `requests` package
 #
 present=`pip list | grep requests | sed 's/^requests\ *//'`
-if test ! $(echo $present)
-then
+if test ! $(echo $present); then
 	echo "   python package requests not found. Cloning git and installing it."
 	mkdir -p /tmp/requests
 	cd /tmp/requests
@@ -36,9 +83,8 @@ fi
 # Installing `lxml` package
 #
 present=`pip list | grep lxml | sed 's/^lxml\ *//'`
-if test ! $(echo $present)
-then
-	sudo -H pip install lxml
+if test ! $(echo $present); then
+	pip install lxml --user
 else
 	echo "   found lxml in version $present"
 fi
@@ -46,9 +92,8 @@ fi
 # Installing `tornado` package
 #
 present=`pip list | grep tornado | sed 's/^tornado\ *//'`
-if test ! $(echo $present)
-then
-	sudo -H pip install tornado
+if test ! $(echo $present); then
+	pip install tornado --user
 else
 	echo "   found tornado in version $present"
 fi
@@ -56,20 +101,18 @@ fi
 # Installing `nose` package
 #
 present=`pip list | grep nose | sed 's/^nose\ *//'`
-if test ! $(echo $present)
-then
-	sudo -H pip install nose
+if test ! $(echo $present); then
+	pip install nose --user
 else
 	echo "   found nose in version $present"
 fi
-
 
 # Installing `botocore` package
 #
 present=`pip list | pcregrep '^botocore ' | sed 's/^botocore\ *//'  | sed 's/\n*//'`
 if test ! $(echo $present)
 then
-	sudo -H pip install botocore
+	pip install botocore --user
 else
 	echo "   found botocore in version $present"
 fi
@@ -79,7 +122,7 @@ fi
 present=`pip list | pcregrep '^boto '| sed 's/^boto\ *//'  | sed 's/\n*//'`
 if test ! $(echo $present)
 then
-	sudo -H pip install boto
+	pip install boto --user
 else
 	echo "   found boto in version $present"
 fi
@@ -89,21 +132,24 @@ fi
 present=`pip list | pcregrep '^boto3 ' | sed 's/^boto3\ *//' | sed 's/\n*//'`
 if test ! $(echo $present)
 then
-	sudo -H pip install boto3
+	pip install boto3 --user
 else
 	echo "   found boto3 in version $present"
 fi
 
 echo
 echo "#----------------------------------------#"
-echo "#     Upgrading all python packages      #"
-echo "#     Keeping docutils at 0.15.2         #"
-echo "#     Keeping pip from updating too      #"
+echo "#     Upgraded python packages           #"
+echo "#     Kept         pip at $pip_version         #"
+echo "#     Kept    docutils at $docutils_version         #"
+echo "#     Kept        idna at $idna_version            #"
+echo "#     Please check periodically if we    #"
+echo "#     can update these packages          #"
 echo "#----------------------------------------#"
 echo " "
 echo " "
 
-pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | grep -v docutils | grep -v pip | xargs -n1 pip install -U --user
+pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | pcregrep -v "$exclude_pattern" | xargs -n1 pip install -U --user
 
 echo " "
 echo " "
@@ -117,9 +163,14 @@ echo
 
 echo
 echo "#----------------------------------------#"
-echo "#   Upgrading all python packages done   #"
-echo "#   Froze docutils and pip               #"
-echo "#         0.15.2       19.3.1            #"
+echo "#     Upgrading all python packages      #"
+echo "#     Keeping      pip at $pip_version         #"
+echo "#     Keeping docutils at $docutils_version         #"
+echo "#     Keeping     idna at $idna_version            #"
+echo "#     Please check periodically if we    #"
+echo "#     can update these packages          #"
+echo "#     eclude pattern was                 #"
+echo "#     $exclude_pattern "
 echo "#   See updatable python packages below  #"
 echo "#----------------------------------------#"
 echo " "
--- vagrant/install.sh
+++ vagrant/install.sh
@@ -4,4 +4,5 @@
 # that are needed for the provisioning of the development vagrant box
 #
 command -v vagrant && vagrant plugin install vagrant-hostmanager
-command -v vagrant && vagrant plugin install vagrant-disksize 
+command -v vagrant && vagrant plugin install vagrant-disksize
+command -v vagrant && vagrant plugin install vagrant-bindfs
