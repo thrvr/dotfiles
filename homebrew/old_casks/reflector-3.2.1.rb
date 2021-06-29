a747a6feec4f61fe41425940f55e29c9eeeb43f2
Dirk Tornow <dirk@thrvrs.com>
2021-06-29 11:01:29 +0200

    added blender

---
+++ homebrew/old_casks/reflector-3.2.1.rb
@@ -0,0 +1,21 @@
+cask "reflector" do
+  version "3.2.1"
+  sha256 "ef56cde06b2aed87b164f2856f5190931d839e552d3b0755db11bddf5db86614"
+  url "https://download.airsquirrels.com/Reflector#{version.major}/Mac/Reflector-#{version.before_comma}.dmg"
+  name "Reflector"
+  desc "Wireless screen-mirroring application"
+  homepage "https://www.airsquirrels.com/reflector/"
+
+  livecheck do
+    url "https://updates-prod.airsquirrels.com/Reflector#{version.major}/Mac/updateCheck/"
+    strategy :sparkle
+  end
+
+  app "Reflector #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Support/Logs/Reflector*.log*",
+    "~/Library/Caches/com.squirrels.Reflector-*",
+    "~/Library/Preferences/com.squirrels.Reflector-*.plist",
+  ]
+end
