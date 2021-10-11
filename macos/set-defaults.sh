4573cbe948049ad3607a5d9b049d39671c04d9de
Dirk Tornow <dirk@thrvrs.com>
2021-10-11 16:50:17 +0200

    added new mac address

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -27,9 +27,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Set computer name (as done via System Preferences → Sharing)
 HostName="UnknownComputer"
 ifconfig -a | grep ce:eb:52 && HostName="BerlinBase"
-ifconfig -a | grep 13:82:e6 && HostName="McDiTo"
+ifconfig -a | grep a4:83:e7:8b:ef:3f && HostName="McDiTo"
 ifconfig -a | grep 8:92:fe && HostName="MacFeather"
-
 
 
 echo "Setting HostName to $HostName"
--- omnigraffle/install.sh
+++ omnigraffle/install.sh
@@ -2,5 +2,6 @@
 # first we save all currently added stencil in our dotfiles
 # then we copy them all back
 
-cp -R ~/Library/Containers/com.omnigroup.OmniGraffle7/Data/Library/Application\ Support/The\ Omni\ Group/OmniGraffle/Stencils/*.gstencil ~/.dotfiles/omnigraffle/stencils/
-cp -R ~/.dotfiles/omnigraffle/stencils/* ~/Library/Containers/com.omnigroup.OmniGraffle7/Data/Library/Application\ Support/The\ Omni\ Group/OmniGraffle/Stencils/
+
+# cp -R ~/Library/Containers/com.omnigroup.OmniGraffle7/Data/Library/Application\ Support/The\ Omni\ Group/OmniGraffle/Stencils/* ~/.dotfiles/omnigraffle/stencils/
+# cp -R ~/.dotfiles/omnigraffle/stencils/* ~/Library/Containers/com.omnigroup.OmniGraffle7/Data/Library/Application\ Support/The\ Omni\ Group/OmniGraffle/Stencils/
